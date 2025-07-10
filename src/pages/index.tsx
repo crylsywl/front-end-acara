d1b32757e0451ad227c94dabbf2fe264bb3268af
crylsywl <crylsyawal123@gmail,com>
2025-07-10 14:10:22 +0700

    Dashboard

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import PageHead from "@/components/commons/PageHead";
 import { Button } from "@heroui/react";
 import { Inter } from "next/font/google";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,8 +10,28 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <PageHead/>
-      <Button color="primary">Button clik</Button>
+      <PageHead />
+      <div className="flex flex-col items-center justify-center gap-4 rounded-lg bg-white p-8 shadow-md">
+        <div className="flex flex-col items-center justify-center gap-2">
+          <h1 className="text-4xl font-bold text-danger-500">
+            Welcome to My Next.js App
+          </h1>
+          <p>This is a simple example of a Next.js application.</p>
+          <p className="max-w-2xl text-center">
+            This project is a dummy authentication system that includes basic
+            features such as user registration and login. It is built using
+            Next.js and designed for learning and demonstration purposes only.
+          </p>
+        </div>
+        <div className="flex flex-row gap-4">
+          <Link href={"/auth/register"}>
+            <Button color="danger">Register</Button>
+          </Link>
+          <Link href={"/auth/login"}>
+            <Button color="danger">Login</Button>
+          </Link>
+        </div>
+      </div>
     </main>
   );
 }
